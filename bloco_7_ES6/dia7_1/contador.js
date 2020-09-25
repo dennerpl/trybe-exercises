9601f8d9f9ffd3a4795907f3d7222994f974dd02
dennerpl <denner.souzapl@gmail.com>
2020-09-25 18:45:23 -0300

    exercício contador de clicks finalizado

---
+++ bloco_7_ES6/dia7_1/contador.js
@@ -0,0 +1,7 @@
+let count = 0
+document.getElementById("buttonCount").addEventListener("click", () => {
+    count += 1
+    const container = document.getElementById("result")
+    container.innerHTML = `Você apertou o botão ${count} vezes`
+
+})
