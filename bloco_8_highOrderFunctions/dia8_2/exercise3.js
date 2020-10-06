73707f5cffc872217a591ba684cbad8ebbcfb4ff
dennerpl <denner.souzapl@gmail.com>
2020-10-06 19:29:03 -0300

    exercise 3 finished

--- bloco_8_highOrderFunctions/dia8_2/exercise3.js
+++ bloco_8_highOrderFunctions/dia8_2/exercise3.js
@@ -64,20 +64,54 @@ const books = [
 ];
 
 const expected_result = [
-  "As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin",
-  "O Senhor dos Anéis - Fantasia - J. R. R. Tolkien",
-  "Fundação - Ficção Científica - Isaac Asimov",
-  "Duna - Ficção Científica - Frank Herbert",
-  "A Coisa - Terror - Stephen King",
-  "O Chamado de Cthulhu - Terror - H. P. Lovecraft",
+  {
+    id: 1,
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
+    author: {
+      name: "George R. R. Martin",
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
+    author: {
+      name: "J. R. R. Tolkien",
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: "Fundação",
+    genre: "Ficção Científica",
+    author: {
+      name: "Isaac Asimov",
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: "Duna",
+    genre: "Ficção Científica",
+    author: {
+      name: "Frank Herbert",
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
 ];
 
-function formatedBookNames() {
+function fantasyOrScienceFiction() {
   // escreva seu código aqui
-  const result = books.map(
-    (item) => `${item.name} - ${item.genre} - ${item.author.name}`
+  const result = books.filter(
+    (item) => item.genre === "Ficção Científica" || item.genre === "Fantasia"
   );
   return result;
 }
 
-assert.deepEqual(formatedBookNames(), expected_result);
+assert.deepEqual(fantasyOrScienceFiction(), expected_result);
